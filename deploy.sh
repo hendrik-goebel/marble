26a37328f9ecc45b8bf85ecb502974d4a1db6f53
Hendrik Göbel <mail@hendrikgoebel.de>
2021-02-26 17:11:58 +0100

    deployment test 11

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir $REMOTE_PATH/test/release;
-tar -xvzf $REMOTE_PATH/test/artifacts.tar.gz --directory $REMOTE_PATH/test/release
-rm $REMOTE_PATH/test/backup -Rf;
-mv $REMOTE_PATH/test/master ./backup;
-mv $REMOTE_PATH/test/release ./master;
-rm $REMOTE_PATH/test/artifacts.tar.gz
